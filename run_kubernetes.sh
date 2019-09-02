e059520c46aec55923cc10c4be003e0e5e8a6232
craigtdwork <craigtdwork@penguin>
2019-09-01 20:40:57 -0400

    Third  update run k8s script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -8,11 +8,14 @@
 dockerpath=cvonderh/udac-devops
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run --image=$dockerpath devops-app --port=8000
+# kubectl run --image=index.docker.io/cvonderh/udac-devops devops-app --port=8000
+# kubectl run --image=nginx devops-app --port=8000
+# kubectl create deployment devops-app --image=index.docker.io/cvonderh/udac-devops --port=8000
+kubectl run --image=nginx devops-app --port=8000
 #kubectl run --image=$dockerpath devops --port=80 --env="DOMAIN=cluster"
 # Step 3:
 # List kubernetes pods
 kubectl get pods
 # Step 4:
 # Forward the container port to a host
-kubectl expose deployment devops-app --port=8080 --name=prediction-http
+# kubectl expose deployment devops-app --port=8080 --name=prediction-http
